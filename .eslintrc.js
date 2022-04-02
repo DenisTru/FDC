c170e1f8c3980b45a400c5c8f727d3e59f7d257e
Denis Truong <denistruong1@gmail.com>
2022-04-01 20:23:53 -0500

    added jest rules for linter and pkg.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:import/react',
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -18,5 +20,18 @@ module.exports = {
     'react',
   ],
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
   },
 };
