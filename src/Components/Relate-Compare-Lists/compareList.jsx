e343dbb3105812fea1fa8db35692aebd628b8966
Jacob Hansen <jacob.a.hansen2@gmail.com>
2022-04-04 17:23:19 -0500

    adds react icons library to use for button icons, removes scss to center elements within index.scss, updates scss for related / comparison lists.

--- src/Components/Relate-Compare-Lists/compareList.jsx
+++ src/Components/Relate-Compare-Lists/compareList.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './relateCompareLists.scss';
 
 const compareList = () => (
-  <div>Your Outfit</div>
+  <div>
+    <p>Your Outfit</p>
+    <div className="slider-container">
+      <div className="arrow-buttons">Left Arrow</div>
+      <div className="slider">Slider</div>
+      <div className="arrow-buttons">Right Arrow</div>
+    </div>
+  </div>
 );
 
 export default compareList;
--- src/Components/Relate-Compare-Lists/relatedList.jsx
+++ src/Components/Relate-Compare-Lists/relatedList.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './relateCompareLists.scss';
 
 const relatedList = () => (
-  <div>Related Products</div>
+  <div>
+    <p>Related Products</p>
+    <div className="slider-container">
+      <div className="arrow-buttons">Left Arrow</div>
+      <div className="slider">Slider</div>
+      <div className="arrow-buttons">Right Arrow</div>
+    </div>
+  </div>
 );
 
 export default relatedList;
