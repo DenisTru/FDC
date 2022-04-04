0d73062ae9e68db9170bb15adc3c944d5631ba2c
Jacob Hansen <jacob.a.hansen2@gmail.com>
2022-04-04 13:08:01 -0500

    Add related / comparison list render values.

--- src/Components/Relate-Compare Lists/Compare_Items_List.jsx
+++ src/Components/Relate-Compare Lists/Compare_Items_List.jsx
@@ -1,1 +1,9 @@
 import React from 'react';
+
+function compareList() {
+  return (
+    <div>Comparison List Here</div>
+  );
+}
+
+export default compareList;
--- src/Components/Relate-Compare Lists/Related_Items_List.jsx
+++ src/Components/Relate-Compare Lists/Related_Items_List.jsx
@@ -1,1 +1,9 @@
 import React from 'react';
+
+function relatedList() {
+  return (
+    <div>Related List Here</div>
+  );
+}
+
+export default relatedList;
