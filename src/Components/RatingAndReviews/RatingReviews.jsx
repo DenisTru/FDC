75206184acb415359d870da1bf71b60c33d44f3f
qiqi1226 <qitang1226@gmail.com>
2022-04-04 20:49:52 -0500

    breakdown functional

--- src/Components/RatingAndReviews/RatingReviews.jsx
+++ src/Components/RatingAndReviews/RatingReviews.jsx
@@ -4,13 +4,21 @@ import RatingReviewsList from './RatingReviewsList';
 import ReviewButtons from './ReviewButtons';
 import reviewPropTypes from './reviewPropTypes';
 import SortOptions from './SortOptions';
+import RatingBreakdown from './RatingBreakdown';
 
 export default function RatingReviews({
   data, helpOnClick, reviewsNextPage, moreReviewsOnClick,
-  onSortChange,
+  onSortChange, Quality, ratings, recommended,
 }) {
   return (
     <div>
+      <div>
+        <RatingBreakdown
+          Quality={Quality}
+          ratings={ratings}
+          recommended={recommended}
+        />
+      </div>
       <div>
         <SortOptions onSortChange={onSortChange} />
       </div>
@@ -44,8 +52,28 @@ RatingReviews.propTypes = {
   reviewsNextPage: PropTypes.arrayOf(reviewPropTypes).isRequired,
   moreReviewsOnClick: PropTypes.func.isRequired,
   onSortChange: PropTypes.func.isRequired,
+  Quality: PropTypes.shape({
+    id: PropTypes.number,
+    value: PropTypes.string,
+  }),
+  ratings: PropTypes.shape({
+    2: PropTypes.string,
+    3: PropTypes.string,
+    4: PropTypes.string,
+    5: PropTypes.string,
+    1: PropTypes.string,
+  }),
+  recommended: PropTypes.shape({
+    false: PropTypes.string,
+    true: PropTypes.string,
+  }),
 };
 RatingReviews.defaultProps = {
+  ratings: {
+    1: '0', 2: '0', 3: '0', 4: '0', 5: '0',
+  },
+  Quality: {},
+  recommended: {},
 };
 
 // export default RatingReviews;
