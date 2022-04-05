29dead676395e4ec9671bb6c7dbc6967c4dfd59f
Jacob Hansen <jacob.a.hansen2@gmail.com>
2022-04-05 13:17:10 -0500

    Render sliders onto page.

--- src/Components/Relate-Compare-Lists/relatedList.jsx
+++ src/Components/Relate-Compare-Lists/relatedList.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './relateCompareLists.scss';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+
+const slides = [1, 2, 3, 4, 5, 6, 7, 8];
 
 const relatedList = () => (
   <div>
     <p>Related Products</p>
     <div className="slider-container">
-      <div className="arrow-buttons">Left Arrow</div>
-      <div className="slider">Slider</div>
-      <div className="arrow-buttons">Right Arrow</div>
+      <MdKeyboardArrowLeft size={40} className="arrow-button-left" />
+      <div className="slider">
+        {slides.map((slide) => <div className="slider-cards">{slide}</div>)}
+      </div>
+      <MdKeyboardArrowRight size={40} className="arrow-button-right" />
     </div>
   </div>
 );
 
 export default relatedList;
-
-// Input = Array
