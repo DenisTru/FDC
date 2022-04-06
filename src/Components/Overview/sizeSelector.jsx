6e651a2d6a7e46c810c90efdea00ad1d915df1d2
Denis Truong <denistruong1@gmail.com>
2022-04-06 18:53:31 -0500

    quantity updates on size select

--- src/Components/Overview/sizeSelector.jsx
+++ src/Components/Overview/sizeSelector.jsx
@@ -5,24 +5,25 @@ class SizeSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: '',
-      quantity: 0,
+      itemStock: '',
+      quantityToPurchase: 0,
     };
   }
 
   handleChangeSize(e) {
-    this.setState({ item: e.target.value }, () => console.log(this.state));
+    this.setState({ itemStock: Number(e.target.value) }, () => console.log(this.state));
   }
 
   handleChangeQuantity(e) {
-
+    this.setState({ quantityToPurchase: e.target.value }, () => console.log(this.state));
   }
 
   render() {
     const { currentSelectedStyle } = this.props;
-    const { item } = this.state;
+    const { itemStock } = this.state;
     const skuOptions = Object.entries(currentSelectedStyle.skus);
     const sumStock = skuOptions.reduce((prev, current) => prev + current[1].quantity, 0);
+
     if (sumStock <= 0) {
       return (
         <div className="selectSize-container">
@@ -39,16 +40,23 @@ class SizeSelector extends React.Component {
         <div className="custom-select">
           <select onChange={(e) => this.handleChangeSize(e)}>
             <option selected disabled>Select Size</option>
-            {skuOptions.map((item) => {
-              if (item[1].quantity > 0) {
-                return <option value={item[0]}>{item[1].size}</option>;
+            {skuOptions.map((itemStock) => {
+              if (itemStock[1].quantity > 0) {
+                return (
+                  <option
+                    value={JSON.stringify(itemStock[1].quantity)}
+                  >
+                    {itemStock[1].size}
+                  </option>
+                );
               }
             })}
           </select>
         </div>
         <div className="custom-select">
-          <select onChange={(e) => this.handleChangeQuantity(e)} disabled={!item}>
+          <select onChange={(e) => this.handleChangeQuantity(e)} disabled={!itemStock}>
             <option selected disabled>-</option>
+            {Array.from(Array(Number(itemStock)), (e, i) => <option key={i}>{i + 1}</option>)}
           </select>
         </div>
       </div>
@@ -76,9 +84,9 @@ class SizeSelector extends React.Component {
 //       <div className="custom-select">
 //         <select>
 //           <option value="" disabled selected>Select Size</option>
-//           {skuOptions.map((item) => {
-//             if (item[1].quantity > 0) {
-//               return <option value={item[1]}>{item[1].size}</option>;
+//           {skuOptions.map((itemStock) => {
+//             if (itemStock[1].quantity > 0) {
+//               return <option value={itemStock[1]}>{itemStock[1].size}</option>;
 //             }
 //           })}
 //         </select>
