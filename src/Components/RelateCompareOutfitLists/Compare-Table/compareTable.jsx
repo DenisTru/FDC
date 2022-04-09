2c89a127f5c79f8a2a7ac9a4d605ef9daa47c6fe
Jacob Hansen <jacob.a.hansen2@gmail.com>
2022-04-09 08:15:01 -0500

    Adds pop up modal to window to be filled with table and content next.

--- src/Components/RelateCompareOutfitLists/Compare-Table/compareTable.jsx
+++ src/Components/RelateCompareOutfitLists/Compare-Table/compareTable.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import './compareTable.scss';
 
 export default function compareTable({ compare, stopComparing }) {
   if (compare) {
-    console.log('COMPARING');
     return (
-      <div> </div>
+      <div className="modal">
+        <div
+          className="overlay"
+          onClick={stopComparing}
+          onKeyPress={stopComparing}
+          role="button"
+          tabIndex={0}
+          aria-label="Close Comparison Table"
+        />
+        <div className="modal-content">
+          <h2>Comparison</h2>
+          <p> Table Info </p>
+          <button className="close-modal" type="button" onClick={stopComparing}>X</button>
+        </div>
+      </div>
     );
   }
-  console.log('NOT COMPARING');
   return (
     <div> </div>
   );
