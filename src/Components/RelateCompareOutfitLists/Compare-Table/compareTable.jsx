38475b83f7c453aa3951ed1b52f65d9d73d85991
Jacob Hansen <jacob.a.hansen2@gmail.com>
2022-04-10 13:48:02 -0500

    Completes all fundmental components for related, compare, and outfit list features. CSS needed.

--- src/Components/RelateCompareOutfitLists/Compare-Table/compareTable.jsx
+++ src/Components/RelateCompareOutfitLists/Compare-Table/compareTable.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './compareModal.scss';
+import GetStars from '../Related-List/Related-Cards/getStars';
 
 export default function compareTable({
   currentProduct, currentProductStyles, currentProductRatingInfo,
   productToCompare, productToCompareStyles, productToCompareRating,
 }) {
+  const currentProductRating = <GetStars ratingValue={currentProductRatingInfo[0].rating} />;
+  const compareProductRating = <GetStars ratingValue={productToCompareRating.rating} />;
+
+  const comparisonInfo = [
+    { currentProduct: '-', category: '-', comparedProduct: '-' },
+    { currentProduct: currentProductRating, category: 'Rating', comparedProduct: compareProductRating },
+    { currentProduct: currentProduct.category, category: 'Category', comparedProduct: productToCompare.category },
+    { currentProduct: currentProduct.default_price, category: 'Price', comparedProduct: productToCompare.default_price },
+    { currentProduct: currentProduct.description, category: 'Description', comparedProduct: productToCompare.description },
+  ];
+
   return (
     <div>
       <table cellSpacing="0" style={{ width: '100%', height: '500px', padding: '5px 10px' }}>
@@ -18,51 +30,13 @@ export default function compareTable({
         </thead>
 
         <tbody id="table-body">
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>Name</td>
-            <td>Name</td>
-          </tr>
+          {comparisonInfo.map((info, index) => (
+            <tr key={JSON.stringify(info + index)}>
+              <td>{info.currentProduct}</td>
+              <td>{info.category}</td>
+              <td>{info.comparedProduct}</td>
+            </tr>
+          ))}
         </tbody>
 
       </table>
