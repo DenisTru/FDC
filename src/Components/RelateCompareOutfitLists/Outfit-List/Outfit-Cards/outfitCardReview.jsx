a48c62a421b0ff62e6a6ed04b42a36f1c8bb6e18
Jacob Hansen <jacob.a.hansen2@gmail.com>
2022-04-11 11:10:17 -0500

    Fixes Outfit List bug

--- src/Components/RelateCompareOutfitLists/Outfit-List/Outfit-Cards/outfitCardReview.jsx
+++ src/Components/RelateCompareOutfitLists/Outfit-List/Outfit-Cards/outfitCardReview.jsx
@@ -5,24 +5,16 @@ import GetStars from '../../Related-List/Related-Cards/getStars';
 export default function outfitCardReview({ productRating }) {
   if (productRating.length && productRating[0].numReviews > 0) {
     return (
-      <div>
-        <div id="productStarRating">
-          <GetStars ratingValue={productRating[0].rating} />
-        </div>
-        <div id="number-of-ratings">
-          {`${`(${productRating[0].numReviews})`}`}
-        </div>
-      </div>
+      <GetStars
+        ratingValue={productRating[0].rating}
+        numRatings={productRating[0].numReviews}
+      />
     );
   }
   return (
-    <div>
-      <div id="productStarRating">
-        <GetStars ratingValue={0} />
-      </div>
-      <div id="number-of-ratings">
-        {`${'(0)'}`}
-      </div>
-    </div>
+    <GetStars
+      ratingValue={0}
+      numRatings={0}
+    />
   );
 }
