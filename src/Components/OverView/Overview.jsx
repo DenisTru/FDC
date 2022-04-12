9d544b96864a29b431b74dfcfb1e190688e66ad8
Denis Truong <truongd@ku.edu>
2022-04-12 10:31:19 -0500

    fixed fullscreen for caraousel

--- src/Components/OverView/Overview.jsx
+++ src/Components/OverView/Overview.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import $ from 'jquery';
 import { BsCheck } from 'react-icons/bs';
 import PropTypes from 'prop-types';
-import { addToCartDELETE, addToCartPOST } from './data';
+import { addToCartPOST } from './data';
 import ItemStyles from './itemStyles';
 import './styles/overview.scss';
 import StarRating from './starRating';
@@ -58,10 +58,7 @@ class Overview extends React.Component {
         addQuantityToCart.push(addToCartPOST(itemSku));
       }
 
-      Promise.all(addQuantityToCart)
-        .then((data) => {
-          console.log(data);
-        });
+      Promise.all(addQuantityToCart);
     }
   };
 
@@ -140,7 +137,7 @@ class Overview extends React.Component {
           <div className="body">
             <div className="product-slogan">{slogan || 's'}</div>
             <div className="product-description">{description || 's'}</div>
-            <SocialShare className="social-share-container" image={currentShownImage} />
+            <SocialShare image={currentShownImage} />
           </div>
         </div>
         <div className="gmo-free">
